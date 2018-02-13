4c21e68416c47ca381a4b2a50e4bc626c0e758d5
Hugo <12248563+Hugobert@users.noreply.github.com>
2018-02-13 15:24:28 +0100

    Design change, show content only when json data is received

--- status.php
+++ status.php
@@ -31,6 +31,7 @@
         text-align:center;
         color:#25292e;
         font-weight:bold;
+        display: none;
       }
       .list-group-item{
         background-color:#25292e
@@ -49,25 +50,35 @@
   </head>
   <body>
     <div class="jumbotron text-center">
-      <p>Detailed status for order #<span id="id"></span></p>
+      <p id="statustext" style="display:none;">
+        Detailed status for order #<span id="id"></span><br>
+        from <span id="date"></span>
+      </p>
+      <hr>
       <div id="loading">
         <h1>Please wait...</h1>
         <div class="progress">
           <div class="progress-bar bg-danger" style="width: 0%"></div>
         </div>
       </div>
-      <h1 id="orderStatus.externalName"></h1>
-      <p id="orderStatus.externalDesc"></p>
-      <p>
-        <span id="firstname"></span> <span id="lastname"></span><br>
-        <span id="address1"></span><br>
-        <span id="zipcode"></span> <span id="city"></span>
-      </p>
+      <div class="row">
+        <div class="col">
+          <h1 id="orderStatus.externalName"></h1>
+          <p id="orderStatus.externalDesc"></p>
+        </div>
+        <div class="col">
+          <p>
+            <span id="firstname"></span> <span id="lastname"></span><br>
+            <span id="address1"></span><br>
+            <span id="zipcode"></span> <span id="city"></span>
+          </p>
+        </div>
+      </div>
     </div>
 
-    <p class="intermediate">Your order contains <span id="allitems.length"></span> items.</p>
+    <p class="intermediate" id="howmanyitems">Your order contains <span id="allitems.length"></span> items.</p>
 
-    <table class="table table-dark table-striped">
+    <table class="table table-dark table-striped" id="customTable" style="display:none;">
       <thead>
         <tr>
           <th>Custom Toys</th>
@@ -78,7 +89,7 @@
       <tbody id="table1">
       </tbody>
     </table>
-    <table class="table table-dark table-striped">
+    <table class="table table-dark table-striped" id="invTable" style="display:none;">
       <thead>
         <tr>
           <th>Inventory Items</th>
@@ -100,10 +111,21 @@
         var getstring = "get.php?id="+"<?php echo $_POST["id"]; ?>"+"&token="+"<?php echo $_POST["token"]; ?>";
         $.getJSON(getstring, function(data) {
           if (data["error"]) {
-            document.getElementById("loading").innerHTML="<strong>There was a problem:</strong><br>"+data["error"];
+            document.getElementById("loading").innerHTML="<strong>There was a problem:</strong><br>"+data["error"]+'<br><a href="index.html"><button type="button" class="btn btn-danger">Try again</button></a>';
           } else {
             document.getElementById("loading").style.display="none";
+            document.getElementById("statustext").style.display="block";
+            document.getElementById("howmanyitems").style.display="block";
             document.getElementById("id").innerHTML=data["id"];
+            var date = new Date(data["created"]);
+            var year = date.getFullYear();
+            var month = withzero(date.getMonth()+1);
+            var day = withzero(date.getDate());
+            var h = withzero(date.getHours());
+            var m = withzero(date.getMinutes());
+            var s = withzero(date.getSeconds());
+            var datestring = year+"-"+month+"-"+day+" at "+h+":"+m;
+            document.getElementById("date").innerHTML=datestring;
             document.getElementById("allitems.length").innerHTML=data["customToys"].length+data["inventoryItems"].length;
             document.getElementById("orderStatus.externalName").innerHTML=data["orderStatus"]["externalName"];
             document.getElementById("orderStatus.externalDesc").innerHTML=data["orderStatus"]["externalDesc"];
@@ -112,6 +134,12 @@
             document.getElementById("address1").innerHTML=lower(data["shippingAddress"]["address1"]);
             document.getElementById("zipcode").innerHTML=data["shippingAddress"]["zipcode"];
             document.getElementById("city").innerHTML=lower(data["shippingAddress"]["city"]);
+            if (data["customToys"].length > 0){
+              document.getElementById("customTable").style.display="table";
+            }
+            if (data["inventoryItems"].length > 0){
+              document.getElementById("invTable").style.display="table";
+            }
             for (var i=0;i<data["customToys"].length;i++) {
               var statuscode=data["customToys"][i]["status"];
               switch (statuscode) {
@@ -225,6 +253,11 @@
         return string.replace(/\w\S*/g, function (word) {
           return word.charAt(0) + word.slice(1).toLowerCase();
         });
+      }
+
+      function withzero(number) {
+        if(number<10){number="0"+number;}
+        return number;
       }
 
     </script>
